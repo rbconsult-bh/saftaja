cd44d379d11999de9a05873bd1f5d890b81e1f46
Yazeed AlKhalaf <yazeed@alkhalaf.dev>
2025-12-19 23:16:43 +0300

    remove tries and workers spec in CI mode

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -17,8 +17,6 @@ export default defineConfig({
   testDir: './specs',
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     baseURL: process.env.BASE_URL || 'http://localhost:8080',
