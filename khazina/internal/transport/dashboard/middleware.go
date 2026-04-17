fccb0d35492252533aa1dd03c0f02526d3389702
Yazeed AlKhalaf <yazeed@alkhalaf.dev>
2026-04-17 23:43:08 +0300

    add more log statements and fix naming

--- khazina/internal/transport/dashboard/middleware.go
+++ khazina/internal/transport/dashboard/middleware.go
@@ -11,11 +11,12 @@ import (
 	saftajacontext "github.com/rbconsult-bh/saftaja/khazina/internal/pkg/context"
 	"github.com/rbconsult-bh/saftaja/khazina/internal/pkg/jwt"
 	"github.com/rbconsult-bh/saftaja/khazina/internal/transport/proto/saftaja/dashboard/auth/v1/authpbv1connect"
+	"github.com/rs/zerolog/log"
 )
 
 var ErrInternal = errors.New("internal server error")
 
-var authlessEndpoints = []string{
+var authlessProcedures = []string{
 	authpbv1connect.AuthServiceInitiateAuthProcedure,
 	authpbv1connect.AuthServiceCompleteAuthProcedure,
 	authpbv1connect.AuthServiceRefreshTokenProcedure,
@@ -27,38 +28,45 @@ func NewAuthTokenInterceptor(tv *jwt.Verifier) connect.UnaryInterceptorFunc {
 			ctx context.Context,
 			req connect.AnyRequest,
 		) (connect.AnyResponse, error) {
-			isAuthlessEndpoint := slices.Contains(authlessEndpoints, req.Spec().Procedure)
-			if isAuthlessEndpoint {
+			isAuthlessProcedure := slices.Contains(authlessProcedures, req.Spec().Procedure)
+			if isAuthlessProcedure {
+				log.Ctx(ctx).Info().Msg("endpoint is authless")
 				return next(ctx, req)
 			}
 
 			authHeader := req.Header().Get("authorization")
 			token, found := strings.CutPrefix(authHeader, "Bearer ")
 			if !found {
+				log.Ctx(ctx).Error().Msg("'Bearer ' prefix not found in authorization header")
 				return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("ensure token has 'Bearer ' before its value in header"))
 			}
 
 			claims, err := tv.Verify(token)
 			if err != nil {
+				log.Ctx(ctx).Error().Err(err).Msg("failed to verify token")
 				return nil, connect.NewError(connect.CodeUnauthenticated, nil)
 			}
 
 			if err := claims.MustBeAccess(); err != nil {
+				log.Ctx(ctx).Error().Err(err).Msg("token sent is not access token")
 				return nil, connect.NewError(connect.CodePermissionDenied, nil)
 			}
 
 			userID, err := uuid.Parse(claims.Subject)
 			if err != nil {
+				log.Ctx(ctx).Error().Err(err).Msg("failed to parse subject claim as uuid")
 				return nil, connect.NewError(connect.CodeInternal, ErrInternal)
 			}
 			ctx = saftajacontext.WithCustomerID(ctx, userID)
 
 			sessionID, err := uuid.Parse(claims.SessionID)
 			if err != nil {
+				log.Ctx(ctx).Error().Err(err).Msg("failed to parse session id claim as uuid")
 				return nil, connect.NewError(connect.CodeInternal, ErrInternal)
 			}
 			ctx = saftajacontext.WithSessionID(ctx, sessionID)
 
+			log.Ctx(ctx).Info().Msg("endpoint is authful, all is good")
 			return next(ctx, req)
 		}
 	}
