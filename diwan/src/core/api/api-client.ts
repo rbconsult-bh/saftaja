0b1be4df2bbf0475aad2044efbb25f0de86c1260
Yazeed AlKhalaf <yazeed@alkhalaf.dev>
2026-04-25 23:01:24 +0300

    implement auth interceptor

--- diwan/src/core/api/api-client.ts
+++ diwan/src/core/api/api-client.ts
@@ -1,9 +1,19 @@
-import { createConnectTransport } from "@connectrpc/connect-web"
+import { createConnectTransport, type ConnectTransportOptions } from "@connectrpc/connect-web"
 import { QueryClient } from "@tanstack/react-query";
 import { authInterceptor } from "./auth-interceptor";
+import { createClient } from "@connectrpc/connect";
+import { AuthService } from "../../gen/saftaja/dashboard/auth/v1/auth_pb";
+
+const baseTransportOpts: ConnectTransportOptions = {
+  baseUrl: "http://localhost:8080",
+};
+
+const nakedTransport = createConnectTransport(baseTransportOpts)
+export const refreshClient = createClient(AuthService, nakedTransport);
+
 
 export const transport = createConnectTransport({
-  baseUrl: "http://localhost:8080",
+  ...baseTransportOpts,
   interceptors: [authInterceptor],
 });
 
--- diwan/src/core/api/auth-interceptor.ts
+++ diwan/src/core/api/auth-interceptor.ts
@@ -1,6 +1,53 @@
-import type { Interceptor } from "@connectrpc/connect";
+import { Code, ConnectError, type Interceptor } from "@connectrpc/connect";
+import { TokenVault } from "../session/token-vault";
+import { useSessionStore } from "../session/store";
+import { refreshClient } from "./api-client";
+
+let refreshPromise: Promise<string> | null = null;
 
 export const authInterceptor: Interceptor = (next) => async (req) => {
-  // TODO: use token manager to get token and inject it into header :D
-  return await next(req);
+  const accessToken = TokenVault.getAccess();
+  if (accessToken) {
+    req.header.set("authorization", `Bearer ${accessToken}`);
+  }
+
+  try {
+    return await next(req);
+  } catch (err) {
+    const isUnauthenticated = err instanceof ConnectError && err.code === Code.Unauthenticated;
+    if (!isUnauthenticated) {
+      throw err;
+    }
+
+    const refreshToken = TokenVault.getRefresh();
+    if (!refreshToken) {
+      useSessionStore.getState().actions.logout();
+      throw err;
+    }
+
+    if (!refreshPromise) {
+      refreshPromise = refreshClient
+        .refreshToken({ refreshToken })
+        .then((resp) => {
+          useSessionStore.getState().actions.login(resp.accessToken, resp.refreshToken);
+          return resp.accessToken;
+        })
+        .catch((refreshErr) => {
+          useSessionStore.getState().actions.logout();
+          throw refreshErr
+        })
+        .finally(() => {
+          refreshPromise = null;
+        });
+    }
+
+    try {
+      const newAccessToken = await refreshPromise;
+
+      req.header.set("authorization", `Bearer ${newAccessToken}`);
+      return next(req);
+    } catch (queueErr) {
+      throw err
+    }
+  }
 }
