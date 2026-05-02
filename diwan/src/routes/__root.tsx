5b12a006d24be16a56fd0c1b82ecacb2902129c7
Yazeed AlKhalaf <yazeed@alkhalaf.dev>
2026-05-02 03:45:34 +0300

    add dev tools

--- diwan/src/routes/__root.tsx
+++ diwan/src/routes/__root.tsx
@@ -1,6 +1,9 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { LanguageProvider } from "@/components/LanguageProvider"
 import { TooltipProvider } from "@/components/ui/tooltip"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+
 
 export const Route = createRootRoute({
   component: () => (
@@ -8,6 +11,9 @@ export const Route = createRootRoute({
       <TooltipProvider>
         <div className="min-h-screen font-sans antialiased">
           <Outlet />
+
+          <TanStackRouterDevtools />
+          <ReactQueryDevtools />
         </div>
       </TooltipProvider>
     </LanguageProvider>
